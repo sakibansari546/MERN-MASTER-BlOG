90623e01410784e66a653b02c4c06648ad0d43b0
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2024-05-29 23:39:29 +0530

    create auth forms ui

---
+++ blogging website - frontend/src/components/input.component.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const InputBox = ({ name, type, id, value, placeholder, icon }) => {
+    const [passwordVisibile, setPasswordVisibile] = useState(false);
+
+    return (
+        <div className="relative w-[100%] mb-4">
+            <input
+                className='input-box '
+                type={type === "password" ? (passwordVisibile ? "text" : "password") : type}
+                id={id}
+                name={name}
+                defaultValue={value}
+                placeholder={placeholder}
+            />
+            <i className={`fi ${icon} input-icon`}></i>
+
+            {
+                type === "password" &&
+                <i
+                    onClick={() => setPasswordVisibile(curr => !curr)}
+                    className={`fi ${passwordVisibile ? "fi-rr-eye" : "fi-rr-eye-crossed"} input-icon left-[auto] right-4 cursor-pointer`}
+                    id="eye"
+                >
+                </i>
+            }
+        </div>
+    )
+}
+
+export default InputBox
