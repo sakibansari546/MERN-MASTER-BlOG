88273d6a2bf08eff6f2aa4a45e267a0231d9ce79
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2024-06-05 02:07:07 +0530

    create trending blogs route

--- server/server.js
+++ server/server.js
@@ -280,6 +280,19 @@ server.get('/latest-blogs', (req, res) => {
         })
 })
 
+server.get('/trending-blogs', (req, res) => {
+    Blog.find({ 'draft': false })
+        .populate('author', "personal_info.username personal_info.fullname personal_info.profile_img -_id")
+        .sort({ 'activity.total_reads': -1, "activity.total_likes": -1, "publishedAt": -1 })
+        .select("blog_id title publishedAt -_id")
+        .limit(5)
+        .then(blogs => {
+            return res.status(200).json({ blogs });
+        }).catch(err => {
+            return res.status(500).json({ 'error': err.message });
+        })
+})
+
 // create blog route
 server.post('/create-blog', verifyJWT, (req, res) => {
     let authorId = req.user;
