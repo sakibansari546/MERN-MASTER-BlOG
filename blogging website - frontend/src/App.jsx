8f31eae5a4bac545b312fa7790461de3865c3a6b
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2024-06-05 00:48:43 +0530

    create inpagenavigation component

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -5,6 +5,7 @@ import UserAuthForm from "./pages/userAuthForm.page";
 import { createContext } from "react";
 import { lookInSession } from './common/session';
 import Editor from './pages/editor.pages';
+import HomePage from './pages/home.page';
 
 export const UserContext = createContext();
 
@@ -21,8 +22,8 @@ const App = () => {
         <UserContext.Provider value={{ userAuth, setUserAuth }}>
             <Routes>
                 <Route path="/editor" element={<Editor />} />
-
                 <Route path="/" element={<Navbar />} >
+                    <Route index element={<HomePage />} />
                     <Route path="/signin" element={<UserAuthForm type='sign-in' />} />
                     <Route path="/signup" element={<UserAuthForm type='sign-up' />} />
                 </Route>
--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import AnimationWrapper from '../common/page-animation'
+import InPageNavigation from '../components/inpage-navigation.component'
 
-const Home = () => {
+const HomePage = () => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <AnimationWrapper>
+                <section className='h-cover flex justify-center gap-10'>
+                    {/* Tatest Blogs */}
+                    <div className='w-full'>
+                        <InPageNavigation routes={['home', 'trending blogs']} defaultHidden={['trending blogs']}>
+                            <h1>Latest Blog</h1>
+                            <h1>Trending Blogs</h1>
+                        </InPageNavigation>
+                    </div>
+
+                    {/* fillters and trending Bsogs */}
+                    <div>
+                        tranding
+                    </div>
+
+                </section>
+            </AnimationWrapper>
+        </>
     )
 }
 
-export default Home
+export default HomePage
